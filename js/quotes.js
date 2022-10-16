4feb393d0af5a2c7523e7d6550ea46f43e5b4d50
ian <dykas@oakland.edu>
2022-10-16 11:22:39 -0400

    changed quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1,14 +1,15 @@
 //List of quotes.
-const quote1 = "I made this for my project already.";
-const quote2 = "Now I have to make something else.";
-const quote3 = "At least it makes this homework easier.";
-const quote4 = "Ahhhhhhhhhhhhhh";
-const quote5 = "Great...";
-const quote6 = "Wonder what I'll do now.";
-const quote7 = "Maybe something to determine the quote instead.";
-const quote8 = "Yeah that should work.";
-const quote9 = "Are these even quotes?";
-const quote10 = "I guess so...";
+const quote1 =
+  "I made this for my project already so I'll have to modify it now.";
+const quote2 = "You know what they say.";
+const quote3 = "If you're in pain, just keep going anyways.";
+const quote4 = "It's ok, things will not get better soon.";
+const quote5 = "Just when you think it's done, it's not.";
+const quote6 = "When things seem free, you will regret everything instantly.";
+const quote7 = "This quote is famous.";
+const quote8 = "JavaScript moment";
+const quote9 = "I have no other quote ideas.";
+const quote10 = "The last quote! We did it.";
 
 //Store quotes into array.
 const quoteList = [
